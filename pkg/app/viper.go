6e0bd52bffe26ac96ce7f5cbd59366ddbcd73c1d
xiewei <ivan.xiewei@foxmail.com>
2024-11-04 13:37:19 +0800

    新增环境配置

--- pkg/app/viper.go
+++ pkg/app/viper.go
@@ -4,13 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path"
-	"strings"
 
 	"golang_project_layout/pkg/app/internal"
 	"golang_project_layout/pkg/global"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
@@ -20,16 +19,22 @@ import (
 func Viper(inputPath ...string) *viper.Viper {
 	var config string
 
-	basePath, _ := os.Getwd()
-	configDir := path.Join(basePath, "configs")
-
 	if len(inputPath) == 0 {
 		flag.StringVar(&config, "c", "", "choose config file.")
 		flag.Parse()
 		if config == "" { // 判断命令行参数是否为空, 优先级: 命令行 > 环境变量 > 默认值
 			if configEnv := os.Getenv(internal.ConfigEnv); configEnv == "" { // 判断 internal.ConfigEnv 常量存储的环境变量是否为空
-				config = internal.ConfigDefaultFile
-				fmt.Printf("您正在使用默认配置,config的路径为%s\n", config)
+				switch gin.Mode() {
+				case gin.DebugMode:
+					config = internal.ConfigDefaultFile
+					fmt.Printf("您正在使用gin模式的%s环境名称,config的路径为%s\n", gin.EnvGinMode, internal.ConfigDefaultFile)
+				case gin.ReleaseMode:
+					config = internal.ConfigReleaseFile
+					fmt.Printf("您正在使用gin模式的%s环境名称,config的路径为%s\n", gin.EnvGinMode, internal.ConfigReleaseFile)
+				case gin.TestMode:
+					config = internal.ConfigTestFile
+					fmt.Printf("您正在使用gin模式的%s环境名称,config的路径为%s\n", gin.EnvGinMode, internal.ConfigTestFile)
+				}
 			} else { // internal.ConfigEnv 常量存储的环境变量不为空 将值赋值于config
 				config = configEnv
 				fmt.Printf("您正在使用%s环境变量,config的路径为%s\n", internal.ConfigEnv, config)
@@ -40,14 +45,11 @@ func Viper(inputPath ...string) *viper.Viper {
 	} else { // 函数传递的可变参数的第一个值赋值于config
 		config = inputPath[0]
 		fmt.Printf("您正在使用func Viper()传递的值,config的路径为%s\n", config)
+
 	}
 
 	v := viper.New()
-	if strings.Contains(config, "/") {
-		v.SetConfigFile(config)
-	} else {
-		v.SetConfigFile(path.Join(configDir, config))
-	}
+	v.SetConfigFile(config)
 	v.SetConfigType("yaml")
 	err := v.ReadInConfig()
 	if err != nil {
@@ -62,7 +64,7 @@ func Viper(inputPath ...string) *viper.Viper {
 		}
 	})
 	if err = v.Unmarshal(&global.GVA_CONFIG); err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	return v
